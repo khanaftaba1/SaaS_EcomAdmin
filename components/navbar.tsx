68efd13a48b8ac2e49f4d9136695acdfb8b48678
Aftab Khan <khanaftaba1@gmail.com>
2024-12-08 19:35:07 -0500

    placeholder

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { auth } from "@clerk/nextjs/server";
-import { UserButton } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { MainNav } from '@/components/main-nav'
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
@@ -9,7 +8,7 @@ import prismadb from '@/lib/prismadb';
 
 
 const Navbar = async() => {
-    const { userId } = auth()
+    const { userId } = useAuth()
 
     if (!userId) {
       redirect('/sign-in')
@@ -24,7 +23,8 @@ const Navbar = async() => {
     return ( 
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <StoreSwitcher items={stores}/>
+                {/* <StoreSwitcher items={stores}/> */}
+                <p>Store Switcher</p>
                 <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton />
